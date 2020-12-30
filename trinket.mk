1f3965c1efa0f828a0148467d935c30a8d9f20a6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-12-31 03:12:07 +0530

    trinket-common: Import powerhint.json from sunfish
    https://android.googlesource.com/device/google/sunfish/+/refs/tags/android-11.0.0_r19/powerhint.json

--- trinket.mk
+++ trinket.mk
@@ -307,6 +307,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
      android.hardware.power-service.xiaomi_trinket-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/vndk/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
