70d50f70fda724851d632ac247fe09eabf70a484
Michael Bestas <mkbestas@lineageos.org>
2021-02-04 02:48:48 -0300

    trinket-common: Stop Building Tinymix
    * This tool can be built manually for debugging cases,
      there is no need to ship it in production
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ia5b9df0598835e66e30eaef7fdec81f91c9a3c5e

--- trinket.mk
+++ trinket.mk
@@ -33,8 +33,7 @@ PRODUCT_PACKAGES += \
     libaudio-resampler \
     libaudioroute \
     libtinycompress \
-    libvolumelistener \
-    tinymix
+    libvolumelistener
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
