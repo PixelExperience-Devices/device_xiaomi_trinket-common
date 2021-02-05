c91cb14963ca517d3d56f6c9a2365532f107abd8
dianlujitao <dianlujitao@lineageos.org>
2021-02-05 05:17:37 -0300

    trinket-common: Remove fake android.hidl.manager@1.0.so
    Change-Id: I131ffca0f592406e9c9e7266ccf85a452eff7c4c

--- trinket.mk
+++ trinket.mk
@@ -167,8 +167,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
